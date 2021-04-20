b104045d3465a0e1c6efae87a24619b16787b288
Paweł Urban <pawel.urban.pmg@gmail.com>
2021-04-20 14:30:58 +0200

    Change js code for more clear

--- js/script.js
+++ js/script.js
@@ -17,16 +17,11 @@ function playGame(playerInput){
 
   /* Komputer */
   let randomNumber = Math.floor(Math.random() * 3 + 1);
-  console.log('Wylosowana liczba to: ' + randomNumber);
-
   let argComputerMove = getMoveName(randomNumber);
 
   printMessage('Mój ruch to: ' + argComputerMove);
 
   /* Gracz */
-  /* let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.'); */
-  console.log('Gracz wpisał: ' + playerInput);
-
   let argPlayerMove = getMoveName(playerInput);
 
   printMessage('Twój ruch to: ' + argPlayerMove);
